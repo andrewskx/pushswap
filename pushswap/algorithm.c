38869477f34a7c59c2257e07e3ca67a3c3dc0404
Andrian BOSCANEAN <anboscan@e1p43.chisinau.42.fr>
2018-01-21 18:47:15 +0200

    upload

--- pushswap/algorithm.c
+++ pushswap/algorithm.c
@@ -6,13 +6,33 @@
 /*   By: anboscan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/21 17:32:23 by anboscan          #+#    #+#             */
-/*   Updated: 2018/01/21 18:26:02 by anboscan         ###   ########.fr       */
+/*   Updated: 2018/01/21 18:44:37 by anboscan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
 
-void	sort_algorithm(t_stack *a, t_stack *b, int flag)
+static	void	sort_algorithm_2(t_stack *a, t_stack *b, int flag, int spec)
+{
+	if (spec)
+		while (!is_empty(b))
+		{
+			write(1, "pb\n", 3);
+			push_a_b(a, b);
+			if (flag)
+				print_stack(a, b);
+		}
+	else if (!spec)
+	{
+		if (!is_sorted(a) && a->min_elem_index == a->index)
+		{
+			push_a_b(b, a);
+			write(1, "pa\n", 3);
+		}
+	}
+}
+
+void			sort_algorithm(t_stack *a, t_stack *b, int flag)
 {
 	while (!is_sorted(a))
 	{
@@ -31,20 +51,9 @@ void	sort_algorithm(t_stack *a, t_stack *b, int flag)
 			swap_a_b(a);
 			write(1, "sa\n", 3);
 		}
-		if (!is_sorted(a) && a->min_elem_index == a->index)
-		{
-			push_a_b(b, a);
-			write(1, "pa\n", 3);
-		}
+		sort_algorithm_2(a, b, flag, 0);
 		find_min(a);
-		if (flag)
-			print_stack(a, b);
+		flag == 1 ? print_stack(a, b) : 0;
 	}
-	while (!is_empty(b))
-	{
-		write(1, "pb\n", 3);
-		push_a_b(a, b);
-		if (flag)
-			print_stack(a, b);
-	}
+	sort_algorithm_2(a, b, flag, 1);
 }
